4c8bcc224e6591f92bda0c2d38b799d435f9a45b
alumno <alumno@utnfra.com.ar>
2020-02-04 12:26:40 -0300

    ejercicios 1 a 4

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,15 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'
+*/
 function mostrar()
 {
-	
+    var nombre;
+
+    nombre = prompt("ingrese aqui su nombre"); // puedes pedir cualquier dato
+
+    alert(nombre);
+    
+
 }
 
